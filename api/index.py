1b44e1fbf0921cf2203ecd8b94289a292fd0f32b
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-11-01 11:20:21 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,7 @@
-from typing import Optional
+from flask import Flask, Response
+app = Flask(__name__)
 
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
